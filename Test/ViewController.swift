7a02b05bb13553b2587d36a70ac2e5916eef9e1f
a.denisov <a.denisov@softteco.com>
2018-12-21 16:07:07 +0300

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Андрей Денисов on 12/21/18.
+//  Copyright © 2018 Андрей Денисов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
